9a7ddb59c7c68b8b88276e0e48b9e114b3b3e5c3
ommakwanaa <om.makwana@einfochips.com>
2022-03-04 13:00:20 +0530

    adding DSA 3rd project

--- dsa/3.cpp
+++ dsa/3.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<queue>
+#include<vector>
+#include<unordered_map>
+
 using namespace std;
 
 //Data Structure to store a binary tree node;
@@ -81,6 +84,79 @@ void printNode(Node* root)
     }
 }
 
+//a specific order from bottom to top
+
+void priNodes(Node* root)
+{
+  //return if the tree is empty
+  if(root ==NULL)
+  {
+   return ;
+   }
+ 
+  //start with level 1
+  int level = 1;
+
+  //create an empty multimap of int associated with multiple values
+  unordered_map<int, vector<int>> map;
+
+  //insert the root node at the first level
+  map[level].push_back(root->data);
+  queue<Node*>q1,q2;
+  
+  if(root->l && root->r)
+  {
+    q1.push(root->l);
+    q2.push(root->r);
+   }
+
+    //loop till queue is empty
+    while(!q1.empty())
+    {
+      level++;
+      int n = q1.size();
+
+       while(n--)
+        {
+            //dequeue front node from the first queue and print it
+            Node* x = q1.front();
+            q1.pop();
+       
+            map[level].push_back(x->data);
+            //enqueue left and right child of 'x' to the first queue
+             if(x->l)
+            {
+                q1.push(x->l);
+            }
+            
+            if(x->r)
+            {
+                q1.push(x->r);
+            }
+            //dequeue front node from the first queue and print it
+            Node* y = q2.front();
+            q2.pop();
+            map[level].push_back(y->data);
+            //enqueue left and right child of 'x' to the first queue
+            if(y->r)
+            {
+                q2.push(y->r);
+            }
+            
+            if(y->l)
+            {
+                q2.push(y->l);
+            }
+         }
+     }
+       //print all nodes present at every level
+        for(int i=map.size();i>0; i--)
+        {
+           for(int j: map[i]){
+             cout<<j<<" ";
+        }
+     }
+ }           
 int main()
 {
     Node* root = new Node(1);
@@ -98,7 +174,9 @@ int main()
     root->r->l->r = new Node(13);
     root->r->r->l = new Node(14);
     root->r->r->r = new Node(15);
-    
+    cout<<"Print Top-Down"<<endl;
     printNode(root);
+    cout<<"\nPrint Bottom-Up"<<endl;
+    priNodes(root);
     return 0;
 }
